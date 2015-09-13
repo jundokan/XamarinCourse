cb2f8c94dcf71c8f6179cfe86214172cb022bf1a
Sergii <jundokan2000@gmail.com>
2015-09-13 18:51:31 +0300

    Add WordCounter Project
    Add WordCounter Project

---
+++ WordCounter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "this is my string";
+            int result = str.CountOfWord();
+
+            Console.WriteLine("u = {0}", result );
+            Console.Read();
+        }
+    }
+}
---
+++ WordCounter/VordCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    public static class WordCounter
+    {
+        public static int CountOfWord(this string incomeString)
+        {
+            return incomeString.Split().Length;
+        }
+    }
+}
